61ea515b5fcca691e876030c17b725a3b16f20a6
abythomas300 <abythomas300@gmail.com>
2025-09-26 17:47:41 +0530

    feat: add basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+function Header() {
+    return(
+        <div className="header-container">
+            <h2>MY BLOG</h2>
+            <p>Welcome to my blog where I share my life journey</p>
+        </div>
+    )
+}
+
+export default Header
