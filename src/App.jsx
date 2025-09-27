4b8eb6c00337c236f714abfc62319bc6bed86120
abythomas300 <abythomas300@gmail.com>
2025-09-27 14:43:12 +0530

    feat(main-content): add basic grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header"
+import MainContent from "./components/MainContent"
 import "./App.css"
 function App() {
     return (
-        <Header />
+       <>
+         <Header />
+         <MainContent />
+       </>
     )
 }
 
