4acb23124e69d7ca244e00c6a0d25da67a78e436
abythomas300 <abythomas300@gmail.com>
2025-09-30 20:06:48 +0530

    style(CardComponent): add proper margin between typography components

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -14,35 +14,43 @@ import Box from '@mui/material/Box';
 export default function CardComponent() {
   return (
     <Card sx={{ p:0, my:4, height: '100%', width: '100%'}}>
+
       <CardMedia
         component="img"
         image="https://cdn.pixabay.com/photo/2024/11/07/03/12/lizard-9179598_1280.jpg"
         title="lizard"
         sx={{ height: 400, objectFit: 'cover', objectPosition: 'top' }}
       />
+
+
       <CardContent>
+
         <Typography gutterBottom variant="h5" component="div">
           Lizard
         </Typography>
-        <Box sx={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: 1}} >
+
           <Typography variant='subtitle1' component="div" sx={{my: 1}}>
-              The Dragon Like Creatures in our neighbourhood,
+            The Dragon Like Creatures in our neighbourhood
           </Typography>
-          <Typography variant='subtitle1' component='div' color='textSecondary'>
-          April 7, 2014
+          <Typography variant='subtitle2' component='div' color='textSecondary'>
+            April 7, 2014
           </Typography>
-        </Box>
-        <Typography variant="body2" sx={{ color: 'text.secondary', mt: 3 }}>
-        Lizard is the common name used for all squamate reptiles other than snakes, encompassing over 7,000 species, 
-        ranging across all continents except Antarctica, as well as most oceanic island chains. The grouping is 
-        paraphyletic as some lizards are more closely related to snakes than they are to other lizards
+
+        <Typography variant="body1" sx={{my: 3 }}>
+          Lizard is the common name used for all squamate reptiles other than snakes, encompassing over 7,000 species, 
+          ranging across all continents except Antarctica, as well as most oceanic island chains. The grouping is 
+          paraphyletic as some lizards are more closely related to snakes than they are to other lizards
         </Typography>
+
       </CardContent>
+
+
       <CardActions>
         <Button size="small" color='secondary'><ThumbUpOutlinedIcon /></Button>
         <Button size="small" color='secondary'><CommentOutlinedIcon /></Button>
         <Button size="small" color='secondary'><ShareOutlinedIcon /></Button>
       </CardActions>
+
     </Card>
   );
 }
