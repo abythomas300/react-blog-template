8b3a35724badf56910f401e64aa0de49b3587032
abythomas300 <abythomas300@gmail.com>
2025-10-01 23:56:27 +0530

    config: add deployment details to vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const REPO_NAME = 'react-blog-template'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${REPO_NAME}/`,
   server: {
     port: 3000,
     open: true,
